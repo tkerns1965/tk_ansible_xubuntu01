867dd496223779fab9fb735c478abccb0a69ffd1
tkerns1965 <tony.kerns.65@gmail.com>
2019-03-25 21:13:12 -0400

    Create gnucash_filedir.py

---
+++ roles/xubuntu01/templates/gnucash_filedir.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+DATA_FILE =  '{{ gnucash_data_file }}'
+PDF_DIR =  '{{ gnucash_pdf_dir }}'
+
+
+# main function
+def main():
+    pass
+# end main()
+
+
+if __name__ == "__main__":
+    main()
